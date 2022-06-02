38ca21194c228ea9728ad324584ae52e64269784
Cristian Silva <cristian.silva@avant.com>
2022-06-02 16:49:18 -0500

    Impl grace period & 7 days range

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ from dotenv import load_dotenv
 from twilio.rest import Client
 from dateutil.relativedelta import relativedelta
 
+
+logging.basicConfig(filename = 'call_recordings.log', encoding = 'utf-8', level = logging.DEBUG, format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 load_dotenv() 
-logging.basicConfig(filename = 'example.log', encoding = 'utf-8', level = logging.DEBUG, format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
-days = int(os.environ['MAX_DAYS_TO_REMAIN'])
+days = os.environ.get("MAX_DAYS_TO_REMAIN")
 
 
 def get_recordings_and_delete(client: Client) -> None:
-  recordings = client.recordings.list(
-                                      date_created_before = date.today(),
-                                      date_created_after = date.today() + relativedelta(days=-days),
-                                    )
+  days_before = None
+  seven_days_before = None
+  if days:
+    days_before = date.today() + relativedelta(days=-int(days))
+    seven_days_before = days_before + relativedelta(days=-7)
+  try:
+    recordings = client.recordings.list(
+                                        date_created_before = days_before,
+                                        date_created_after = seven_days_before,
+                                      )
+  except Exception as e:
+    recordings = []
+    logging.error(e)
 
   for record in recordings:
-      if record.status == 'completed':
-        record.delete()
-        logging.info(f'The Recording SID {record.sid} was deleted.')
+    if record.status == 'completed':
+      record.delete()
+      logging.info(f'The Recording SID {record.sid} was deleted.')
 
 
 client = Client(account_sid, auth_token)
